f5a60a34bbdaea922fcfea8a2d8db2a340f4a1ea
Rishita7281 <119398306+Rishita7281@users.noreply.github.com>
2023-05-17 15:46:53 +0530

    solved the issue which occured in connection of database

--- MedAppoint/backend/connection.js
+++ MedAppoint/backend/connection.js
@@ -1,5 +1,5 @@
 const {MongoClient} = require("mongodb")
-const url ='mongodb://localhost:27017'
+const url ='mongodb://127.0.0.1:27017'
 const client = new MongoClient(url)
 const database = 'MedAppoint'
 async function getData(){
